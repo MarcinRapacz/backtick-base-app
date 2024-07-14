08fb674135807daad62d4ea7754ee8454bfba07d
m.rapacz <m.rapacz@getprintbox.com>
2024-07-14 20:25:02 +0200

    change prod docker image

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -115,7 +115,7 @@ export default function Index() {
     <Page>
       <TitleBar title="Remix app template">
         <button variant="primary" onClick={generateProduct}>
-          Generate a product by backtick 0.0.7
+          Generate a product by backtick 0.0.8
         </button>
       </TitleBar>
       <BlockStack gap="500">
